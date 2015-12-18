e7725d0c07887ea212db23e00ff1734ddbe6f2e1
Yin Kokpheng <yinkokpheng@yahoo.com>
2015-12-18 11:02:45 +0700

    123

--- src/main/java/com/FirstSpringProject/App.java
+++ src/main/java/com/FirstSpringProject/App.java
@@ -1,13 +1,16 @@
 package com.FirstSpringProject;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+       
+       Restaurant restaurant = (Restaurant) context.getBean("restaurantBean");
+       
+       restaurant.greetCustomer();
     }
 }
